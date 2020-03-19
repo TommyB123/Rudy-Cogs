d1169ee7bf3db2b2ffbd18641b8ff5260cc3f11f
TommyB <tommyb@frostgaming.com>
2020-03-19 06:57:38 -0500

    Deprecate old !weeb command and add new API

--- cogs/commands_weeb.py
+++ cogs/commands_weeb.py
@@ -4,7 +4,6 @@ import json
 from discord.ext import commands
 
 weebs = [87582156741681152, 253685655471783936, 273956905397911553, 413452980470415361] #tommy, graber, cave & amir
-weebcategories = ['neko', 'kitsune', 'hug', 'pat', 'waifu', 'cry', 'kiss']
 
 async def isweeb(ctx):
 	if ctx.author.id in weebs:
@@ -19,15 +18,18 @@ class WeebCog(commands.Cog, name = "Weeb Commands"):
 	@commands.command(help = "Bad")
 	@commands.guild_only()
 	@commands.check(isweeb)
-	@commands.cooldown(1, 60)
-	async def weeb(self, ctx, category:str = ''):
-		category = category.lower()
-		if category not in weebcategories:
-			await ctx.send('Invalid category. Valid categories: {0}.'.format(', '.join(weebcategories)))
-			return
-
+	async def weeb(self, ctx):
 		async with httpx.AsyncClient() as client:
-			response = await client.get(f'https://neko-love.xyz/api/v1/{category}')
+			response = await client.get('https://nekos.life/api/v2/img/neko')
+			await ctx.send(response.json()['url'])
+
+	@commands.command(help = "Bad (old)")
+	@commands.guild_only()
+	@commands.check(isweeb)
+	@commands.cooldown(1, 60)
+	async def oldweeb(self, ctx):
+		async with httpx.AsyncClient() as client:
+			response = await client.get('https://neko-love.xyz/api/v1/neko')
 			await ctx.send(response.json()['url'])
 
 	@commands.command(help = "Really bad")
