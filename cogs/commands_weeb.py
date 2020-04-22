705bc1b351ee9bc43803a44009c9f12f1d29570b
TommyB <tommyb@frostgaming.com>
2020-04-21 22:46:09 -0500

    Replace httpx with aiohttp

--- cogs/commands_weeb.py
+++ cogs/commands_weeb.py
@@ -1,15 +1,20 @@
 import discord
-import httpx
+import aiohttp
 import json
 from discord.ext import commands
 
-weebs = [87582156741681152, 253685655471783936, 273956905397911553, 413452980470415361] #tommy, graber, cave & amir
+#tommy, graber, cave & amir
+weebs = [87582156741681152, 253685655471783936, 273956905397911553, 413452980470415361]
 
 async def isweeb(ctx):
 	if ctx.author.id in weebs:
 		return True
 	else:
 		return False
+
+async def fetchweeb(session, url):
+	async with session.get(url) as response:
+		return await response.text()
 
 class WeebCog(commands.Cog, name = "Weeb"):
 	def __init__(self, bot):
@@ -19,27 +24,30 @@ class WeebCog(commands.Cog, name = "Weeb"):
 	@commands.guild_only()
 	@commands.check(isweeb)
 	async def weeb(self, ctx):
-		async with httpx.AsyncClient() as client:
-			response = await client.get('https://nekos.life/api/v2/img/neko')
-			await ctx.send(response.json()['url'])
+		async with aiohttp.ClientSession() as session:
+			response = await fetchweeb(session, 'https://nekos.life/api/v2/img/neko')
+			image = json.loads(response)
+			await ctx.send(image['url'])
 
 	@commands.command(help = "Bad (old)")
 	@commands.guild_only()
 	@commands.check(isweeb)
 	@commands.cooldown(1, 60)
 	async def oldweeb(self, ctx):
-		async with httpx.AsyncClient() as client:
-			response = await client.get('https://neko-love.xyz/api/v1/neko')
-			await ctx.send(response.json()['url'])
+		async with aiohttp.ClientSession() as session:
+			response = await fetchweeb(session, 'https://neko-love.xyz/api/v1/neko')
+			image = json.loads(response)
+			await ctx.send(image['url'])
 
 	@commands.command(help = "Really bad")
 	@commands.guild_only()
 	@commands.is_nsfw()
 	@commands.check(isweeb)
 	async def lewdweeb(self, ctx):
-		async with httpx.AsyncClient() as client:
-			response = await client.get('https://neko-love.xyz/api/v1/nekolewd')
-			await ctx.send(response.json()['url'])
+		async with aiohttp.ClientSession() as session:
+			response = await fetchweeb(session, 'https://neko-love.xyz/api/v1/nekolewd')
+			image = json.loads(response)
+			await ctx.send(image['url'])
 
 def setup(bot):
 	bot.add_cog(WeebCog(bot))
--- cogs/help.py
+++ cogs/help.py
@@ -5,15 +5,14 @@ class MyHelpCommand(commands.MinimalHelpCommand):
     def get_command_signature(self, command):
         return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
 
-class MyCog(commands.Cog):
+class HelpCog(commands.Cog):
     def __init__(self, bot):
         self._original_help_command = bot.help_command
         bot.help_command = MyHelpCommand()
         bot.help_command.cog = self
-        bot.help_command.hidden = True
 
     def cog_unload(self):
         self.bot.help_command = self._original_help_command
 
 def setup(bot):
-    bot.add_cog(MyCog(bot))
+    bot.add_cog(HelpCog(bot))
