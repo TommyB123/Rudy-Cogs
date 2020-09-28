94e23d3932e8de684951035f829bb96f46be7ebc
TommyB <tommyb@frostgaming.com>
2020-09-28 11:15:34 -0500

    Group various RCRP staff commands

--- rudyrcrpstaff/rudyrcrpstaff.py
+++ rudyrcrpstaff/rudyrcrpstaff.py
@@ -22,29 +22,20 @@ class RCRPStaffCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.group()
     @commands.guild_only()
     @commands.check(rcrp_check)
     @commands.check(admin_check)
-    async def clear(self, ctx: commands.Context, amount: int):
-        """Clear up to the last 10 messages"""
-        if amount <= 0:
-            await ctx.send("Invalid clear count.")
-            return
+    async def search(self, ctx: commands.Context):
+        """Various search functions"""
+        pass
 
-        if amount > 10:
-            await ctx.send("You cannot clear more than 10 messages at once.")
-            return
-
-        messages = await ctx.channel.history(limit = amount + 1).flatten()
-        await ctx.channel.delete_messages(messages)
-
-    @commands.command()
+    @search.command()
     @commands.guild_only()
     @commands.check(rcrp_check)
     @commands.check(admin_check)
-    async def whois(self, ctx: commands.Context, discord_user: discord.User):
-        """Fetches MA info of a verified discord member"""
+    async def discord(self, ctx: commands.Context, discord_user: discord.User):
+        """Fetches Master Account info for a verified Discord member"""
         sql = await aiomysql.connect( **mysqlconfig)
         cursor = await sql.cursor()
         await cursor.execute("SELECT id, Username, UNIX_TIMESTAMP(RegTimeStamp) AS RegStamp, LastLog FROM masters WHERE discordid = %s", (discord_user.id, ))
@@ -66,12 +57,12 @@ class RCRPStaffCommands(commands.Cog):
         embed.add_field(name = "Last Login Date", value = datetime.utcfromtimestamp(data[3]).strftime('%Y-%m-%d %H:%M:%S'), inline = False)
         await ctx.send(embed = embed)
 
-    @commands.command()
+    @search.command()
     @commands.guild_only()
     @commands.check(rcrp_check)
     @commands.check(admin_check)
-    async def find(self, ctx: commands.Context, master_name: str):
-        """Fetches the discord account of a member based on their MA name"""
+    async def master(self, ctx: commands.Context, master_name: str):
+        """Fetches a Discord account based on a Master Account name search"""
         sql = await aiomysql.connect( **mysqlconfig)
         cursor = await sql.cursor()
         await cursor.execute("SELECT id, discordid, UNIX_TIMESTAMP(RegTimeStamp) AS RegStamp, LastLog FROM masters WHERE Username = %s", (master_name, ))
@@ -104,113 +95,8 @@ class RCRPStaffCommands(commands.Cog):
         embed.add_field(name = "Registration Date", value = datetime.utcfromtimestamp(data[2]).strftime('%Y-%m-%d %H:%M:%S'), inline = False)
         embed.add_field(name = "Last Login Date", value = datetime.utcfromtimestamp(data[3]).strftime('%Y-%m-%d %H:%M:%S'), inline = False)
         await ctx.send(embed = embed)
-
-    @commands.command(help = "Bans a member from the RCRP discord")
-    @commands.guild_only()
-    @commands.check(rcrp_check)
-    @commands.check(admin_check)
-    async def rcrpban(self, ctx: commands.Context, target: discord.Member, *, banreason: str):
-        bannedmember = ctx.guild.get_member(target.id)
-        if member_is_admin(bannedmember):
-            await ctx.send("You can't ban other staff idiot boy.")
-            return
-
-        try:
-            adminuser = await self.bot.fetch_user(ctx.author.id)
-            embed = discord.Embed(title = 'Banned', description = f'You have been banned from the Red County Roleplay Discord server by {adminuser.name}', color = 0xe74c3c, timestamp = ctx.message.created_at)
-            embed.add_field(name = 'Ban Reason', value = banreason)
-            await target.send(embed = embed)
-        except: #an exception will be raised if the bot can't DM the target, so we'll just pass and pretend it never happened
-            pass
-
-        baninfo = f"{banreason} - Banned by {adminuser.name}"
-        await ctx.guild.ban(bannedmember, reason = baninfo, delete_message_days = 0)
-        await ctx.send(f"{bannedmember.mention} has been successfully banned.")
-
-    @commands.command(help = "Unbans a member from the RCRP discord")
-    @commands.guild_only()
-    @commands.check(rcrp_check)
-    @commands.check(admin_check)
-    async def removeban(self, ctx: commands.Context, target_discordid: int):
-        banned_user = await self.bot.fetch_user(target_discordid)
-        if not banned_user:
-            await ctx.send("Invalid user. Enter their discord ID, nothing else.")
-            return
-
-        bans = await ctx.guild.bans()
-        for ban in bans:
-            if ban.user.id == banned_user.id:
-                await ctx.guild.unban(ban.user)
-                await ctx.send(f"{ban.user.mention} has been successfully unbanned")
-                return
-
-        await ctx.send("Could not find any bans for that user.")
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.check(rcrp_check)
-    @commands.check(admin_check)
-    async def baninfo(self, ctx: commands.Context, target_discordid: int):
-        """Searches all existing bans for a banned user."""
-        banned_user: discord.User = await self.bot.fetch_user(target_discordid)
-        if banned_user == None:
-            await ctx.send("Invalid user.")
-            return
-
-        bans = await ctx.guild.bans()
-        for ban in bans:
-            if ban.user.id == banned_user.id:
-                await ctx.send(f"{ban.user.mention} was banned for the following reason: {ban.reason}")
-                return
-        await ctx.send("Could not find any ban info for that user.")
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.check(rcrp_check)
-    @commands.check(admin_check)
-    async def rcrpmute(self, ctx: commands.Context, member: discord.Member):
-        """Assigns the muted role to a discord member"""
-        if member_is_admin(member):
-            await ctx.send("You can't mute other staff.")
-            return
-
-        if member_is_muted(member):
-            await ctx.send(f"{member.mention} is already muted.")
-            return
-
-        await member.add_roles(ctx.guild.get_role(mutedrole))
-        await ctx.send(f"{member.mention} has been muted.")
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.check(rcrp_check)
-    @commands.check(admin_check)
-    async def rcrpunmute(self, ctx: commands.Context, member: discord.Member):
-        """Removes the muted role from a discord member"""
-        if member_is_admin(member):
-            await ctx.send("You can't mute other staff.")
-            return
-
-        if not member_is_muted(member):
-            await ctx.send(f"{member.mention} is not muted.")
-            return
-
-        await member.remove_roles(ctx.guild.get_role(mutedrole))
-        await ctx.send(f"{member.mention} has been unmuted.")
-
-    @commands.command(help = "Sends a message as Rudy")
-    @commands.guild_only()
-    @commands.check(rcrp_check)
-    @commands.check(management_check)
-    async def speak(self, ctx: commands.Context, *, copymessage: str):
-        """Sends a Discord message as Rudy"""
-        if len(copymessage) == 0:
-            return
-
-        await ctx.message.delete()
-        await ctx.send(copymessage)
-
-    @commands.command()
+    
+    @search.command()
     @commands.guild_only()
     @commands.check(rcrp_check)
     @commands.check(admin_check)
@@ -245,7 +131,7 @@ class RCRPStaffCommands(commands.Cog):
         embed.add_field(name = "Ext Furn Limit", value = house['ExteriorFurnLimit'], inline = False)
         await ctx.send(embed = embed)
 
-    @commands.command()
+    @search.command()
     @commands.guild_only()
     @commands.check(rcrp_check)
     @commands.check(admin_check)
@@ -280,13 +166,141 @@ class RCRPStaffCommands(commands.Cog):
         embed.add_field(name = "Special Int", value = 'Yes' if bizz['IsSpecial'] == 1 else 'No', inline = False)
         embed.add_field(name = "Loaned", value = 'Yes' if bizz['Loaned'] == 1 else 'No', inline = False)
         await ctx.send(embed = embed)
-
-    @commands.command()
+    
+    @search.command()
     @commands.guild_only()
+    @commands.check(rcrp_check)
     @commands.check(admin_check)
-    async def avatar(self, ctx: commands.Context, member: discord.Member):
-        """Fetches the avatar of a Discord member"""
-        await ctx.send(f'Avatar of {member.mention}: {member.avatar_url}')
+    async def accountweapons(self, ctx: commands.Context, master_name: str):
+        """Collects a list of all the weapons that an account owns"""
+        master_id = await fetch_account_id(master_name)
+        if master_id == 0:
+            await ctx.send('Invalid account name.')
+            return
+
+        sql = await aiomysql.connect( **mysqlconfig)
+        cursor = await sql.cursor(aiomysql.DictCursor)
+        await cursor.execute("SELECT WeaponID AS weapon, COUNT(*) AS count FROM weapons WHERE OwnerSQLID IN (SELECT id FROM players WHERE MasterAccount = %s) AND Deleted = 0 GROUP BY WeaponID", (master_id, ))
+
+        if cursor.rowcount == 0:
+            await ctx.send(f'{master_name} does not have any weapons.')
+            await cursor.close()
+            sql.close()
+            return
+
+        data = await cursor.fetchall()
+        await cursor.close()
+        sql.close()
+
+        total = 0
+        embed = discord.Embed(title = f'Weapons of {master_name}', color = 0xe74c3c, timestamp = ctx.message.created_at)
+        for weapon in data:
+            embed.add_field(name = weaponnames[weapon['weapon']], value = '{:,}'.format(weapon['count']))
+            total += weapon['count']
+        embed.add_field(name = 'Total Weapons', value = '{:,}'.format(total))
+        await ctx.send(embed = embed)
+
+    @commands.group()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(admin_check)
+    async def punish(self, ctx: commands.Context):
+        pass
+
+    @punish.command()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(admin_check)
+    async def ban(self, ctx: commands.Context, target: discord.Member, *, banreason: str):
+        """Issues a ban to a member of the RCRP discord"""
+        bannedmember = ctx.guild.get_member(target.id)
+        if member_is_admin(bannedmember):
+            await ctx.send("You can't ban other staff idiot boy.")
+            return
+
+        try:
+            adminuser = await self.bot.fetch_user(ctx.author.id)
+            embed = discord.Embed(title = 'Banned', description = f'You have been banned from the Red County Roleplay Discord server by {adminuser.name}', color = 0xe74c3c, timestamp = ctx.message.created_at)
+            embed.add_field(name = 'Ban Reason', value = banreason)
+            await target.send(embed = embed)
+        except: #an exception will be raised if the bot can't DM the target, so we'll just pass and pretend it never happened
+            pass
+
+        baninfo = f"{banreason} - Banned by {adminuser.name}"
+        await ctx.guild.ban(bannedmember, reason = baninfo, delete_message_days = 0)
+        await ctx.send(f"{bannedmember.mention} has been successfully banned.")
+
+    @punish.command()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(admin_check)
+    async def unban(self, ctx: commands.Context, target_discordid: int):
+        """Removes a ban from the ban list"""
+        banned_user = await self.bot.fetch_user(target_discordid)
+        if not banned_user:
+            await ctx.send("Invalid user. Enter their discord ID, nothing else.")
+            return
+
+        bans = await ctx.guild.bans()
+        for ban in bans:
+            if ban.user.id == banned_user.id:
+                await ctx.guild.unban(ban.user)
+                await ctx.send(f"{ban.user.mention} has been successfully unbanned")
+                return
+
+        await ctx.send("Could not find any bans for that user.")
+
+    @punish.command()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(admin_check)
+    async def searchban(self, ctx: commands.Context, target_discordid: int):
+        """Searches all existing bans for a banned user"""
+        banned_user: discord.User = await self.bot.fetch_user(target_discordid)
+        if banned_user == None:
+            await ctx.send("Invalid user.")
+            return
+
+        bans = await ctx.guild.bans()
+        for ban in bans:
+            if ban.user.id == banned_user.id:
+                await ctx.send(f"{ban.user.mention} was banned for the following reason: {ban.reason}")
+                return
+        await ctx.send("Could not find any ban info for that user.")
+
+    @punish.command()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(admin_check)
+    async def mute(self, ctx: commands.Context, member: discord.Member):
+        """Assigns the muted role to a discord member"""
+        if member_is_admin(member):
+            await ctx.send("You can't mute other staff.")
+            return
+
+        if member_is_muted(member):
+            await ctx.send(f"{member.mention} is already muted.")
+            return
+
+        await member.add_roles(ctx.guild.get_role(mutedrole))
+        await ctx.send(f"{member.mention} has been muted.")
+
+    @punish.command()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(admin_check)
+    async def unmute(self, ctx: commands.Context, member: discord.Member):
+        """Removes the muted role from a discord member"""
+        if member_is_admin(member):
+            await ctx.send("You can't mute other staff.")
+            return
+
+        if not member_is_muted(member):
+            await ctx.send(f"{member.mention} is not muted.")
+            return
+
+        await member.remove_roles(ctx.guild.get_role(mutedrole))
+        await ctx.send(f"{member.mention} has been unmuted.")
 
     @commands.group()
     @commands.guild_only()
@@ -409,38 +423,6 @@ class RCRPStaffCommands(commands.Cog):
             await member.add_roles(flrole)
             await ctx.send(f'{member.mention} now has the faction leader role')
     
-    @commands.command(help = "List all guns that a Master Account owns")
-    @commands.guild_only()
-    @commands.check(rcrp_check)
-    @commands.check(admin_check)
-    async def checkweapons(self, ctx: commands.Context, master_name: str):
-        master_id = await fetch_account_id(master_name)
-        if master_id == 0:
-            await ctx.send('Invalid account name.')
-            return
-
-        sql = await aiomysql.connect( **mysqlconfig)
-        cursor = await sql.cursor(aiomysql.DictCursor)
-        await cursor.execute("SELECT WeaponID AS weapon, COUNT(*) AS count FROM weapons WHERE OwnerSQLID IN (SELECT id FROM players WHERE MasterAccount = %s) AND Deleted = 0 GROUP BY WeaponID", (master_id, ))
-
-        if cursor.rowcount == 0:
-            await ctx.send(f'{master_name} does not have any weapons.')
-            await cursor.close()
-            sql.close()
-            return
-
-        data = await cursor.fetchall()
-        await cursor.close()
-        sql.close()
-
-        total = 0
-        embed = discord.Embed(title = f'Weapons of {master_name}', color = 0xe74c3c, timestamp = ctx.message.created_at)
-        for weapon in data:
-            embed.add_field(name = weaponnames[weapon['weapon']], value = '{:,}'.format(weapon['count']))
-            total += weapon['count']
-        embed.add_field(name = 'Total Weapons', value = '{:,}'.format(total))
-        await ctx.send(embed = embed)
-    
     @commands.command()
     @commands.guild_only()
     @commands.check(rcrp_check)
@@ -460,3 +442,39 @@ class RCRPStaffCommands(commands.Cog):
 
         message = ''.join(message)
         await ctx.send(message)
+    
+    @commands.command()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(admin_check)
+    async def clear(self, ctx: commands.Context, amount: int):
+        """Clear up to the last 10 messages"""
+        if amount <= 0:
+            await ctx.send("Invalid clear count.")
+            return
+
+        if amount > 10:
+            await ctx.send("You cannot clear more than 10 messages at once.")
+            return
+
+        messages = await ctx.channel.history(limit = amount + 1).flatten()
+        await ctx.channel.delete_messages(messages)
+    
+    @commands.command()
+    @commands.guild_only()
+    @commands.check(rcrp_check)
+    @commands.check(management_check)
+    async def speak(self, ctx: commands.Context, *, copymessage: str):
+        """Sends a Discord message as Rudy"""
+        if len(copymessage) == 0:
+            return
+
+        await ctx.message.delete()
+        await ctx.send(copymessage)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.check(admin_check)
+    async def avatar(self, ctx: commands.Context, member: discord.Member):
+        """Fetches the avatar of a Discord member"""
+        await ctx.send(f'Avatar of {member.mention}: {member.avatar_url}')
