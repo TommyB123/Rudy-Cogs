19f2ec2a5e1208ed3455ff84280cde1106d0bdbf
TommyB <tommyb@frostgaming.com>
2020-10-06 17:47:47 -0500

    Weeb updates

--- rudyweeb/rudyweeb.py
+++ rudyweeb/rudyweeb.py
@@ -2,15 +2,16 @@ import discord
 import aiohttp
 import json
 import redbot
+import random
 from redbot.core import commands, Config
+from redbot.core.utils.chat_formatting import humanize_list
 
 degenerate_categories = [
-    'femdom', 'tickle', 'classic', 'ngif', 'erofeet', 'meow', 'erok', 'poke', 'les', 'v3', 'hololewd', 'lewdk', 'keta',
+    'femdom', 'tickle', 'classic', 'ngif', 'erofeet', 'erok', 'poke', 'les', 'v3', 'hololewd', 'lewdk', 'keta',
     'feetg', 'nsfw_neko_gif', 'eroyuri', 'kiss', '8ball', 'kuni', 'tits', 'pussy_jpg', 'cum_jpg', 'pussy', 'lewdkemo',
-    'lizard', 'slap', 'lewd', 'cum', 'cuddle', 'spank', 'smallboobs', 'goose', 'random_hentai_gif', 'avatar', 'fox_girl',
-    'nsfw_avatar', 'hug', 'gecg', 'boobs', 'pat', 'feet', 'smug', 'kemonomimi', 'solog', 'holo', 'wallpaper', 'bj', 'woof',
-    'yuri', 'trap', 'anal', 'baka', 'blowjob', 'holoero', 'feed', 'neko', 'gasm', 'hentai', 'futanari', 'ero', 'solo', 'waifu',
-    'pwankg', 'eron', 'erokemo'
+    'slap', 'lewd', 'cum', 'cuddle', 'spank', 'smallboobs', 'random_hentai_gif', 'avatar', 'fox_girl', 'nsfw_avatar',
+    'hug', 'gecg', 'boobs', 'pat', 'feet', 'smug', 'kemonomimi', 'solog', 'holo', 'wallpaper', 'bj', 'yuri', 'trap', 'anal',
+    'baka', 'blowjob', 'holoero', 'feed', 'neko', 'gasm', 'hentai', 'futanari', 'ero', 'solo', 'waifu', 'pwankg', 'eron', 'erokemo'
 ]
 
 async def fetchweeb(session: aiohttp.ClientSession, url: str):
@@ -60,8 +61,11 @@ class WeebCommands(commands.Cog, name = "Weeb"):
     @commands.check(isweeb)
     async def degenerate(self, ctx: commands.Context, *, category: str = ""):
         """The worst"""
+        if category == '*':
+            category = random.choice(degenerate_categories)
+
         if category not in degenerate_categories:
-            await ctx.send(f"Not a valid degenerate category. Valid categories: {degenerate_categories}")
+            await ctx.send(f"Not a valid degenerate category '*' can be used to grab a random category. Valid categories: {humanize_list(degenerate_categories)}")
             return
 
         async with aiohttp.ClientSession() as session:
@@ -69,28 +73,54 @@ class WeebCommands(commands.Cog, name = "Weeb"):
             image = json.loads(response)
             await ctx.send(image['url'])
     
-    @commands.command(help = "Register a dude as a weeb :)")
+    @commands.command()
     @commands.guild_only()
-    @commands.is_owner()
-    async def registerweeb(self, ctx: commands.Context, member: discord.Member):
-        weebs = await self.config.weebs()
-        if member.id in weebs:
-            await ctx.send("This member is already a weeb.")
-            return
-        
-        weebs.append(member.id)
-        await self.config.weebs.set(weebs)
-        await ctx.send(f'{member.mention} is now a certified weeb!')
+    async def lizard(self, ctx: commands.Context):
+        """Delivers a lizard to your front door"""
+        async with aiohttp.ClientSession() as session:
+            response = await fetchweeb(session, 'https://nekos.life/api/v2/img/lizard')
+            image = json.loads(response)
+            await ctx.send(image['url'])
     
-    @commands.command(help = "Banishes a weeb")
+    @commands.command()
+    @commands.guild_only()
+    async def woof(self, ctx: commands.Context):
+        """Puts a nice pup out on display"""
+        async with aiohttp.ClientSession() as session:
+            response = await fetchweeb(session, 'https://nekos.life/api/v2/img/woof')
+            image = json.loads(response)
+            await ctx.send(image['url'])
+    
+    @commands.command()
+    @commands.guild_only()
+    async def meow(self, ctx: commands.Context):
+        """Puts a nice cat out on display"""
+        async with aiohttp.ClientSession() as session:
+            response = await fetchweeb(session, 'https://nekos.life/api/v2/img/meow')
+            image = json.loads(response)
+            await ctx.send(image['url'])
+    
+    @commands.command()
+    @commands.guild_only()
+    async def goose(self, ctx: commands.Context):
+        """goose"""
+        async with aiohttp.ClientSession() as session:
+            response = await fetchweeb(session, 'https://nekos.life/api/v2/img/goose')
+            image = json.loads(response)
+            await ctx.send(image['url'])
+    
+    @commands.command()
     @commands.guild_only()
     @commands.is_owner()
-    async def banishweeb(self, ctx: commands.Context, member: discord.Member):
-        weebs = await self.config.weebs()
-        if member.id not in weebs:
-            await ctx.send("This member is not a weeb.")
-            return
+    async def makeweeb(self, ctx: commands.Context, member: discord.Member):
+        """Certifies or banishes a dude from the weebs"""
+        weebs: list = await self.config.weebs()
 
-        weebs.remove(member.id)
+        if target.id in weebs:
+            weebs.remove(target.id)
+            await ctx.send(f'{target.mention} has been banished from the weebs.')
+        else:
+            weebs.append(target.id)
+            await ctx.send(f'{target.mention} is now a certified weeb!')
+        
         await self.config.weebs.set(weebs)
-        await ctx.send(f'{member.mention} has been banished from the weebs.')
