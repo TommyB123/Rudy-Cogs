b73a8374abe7706de3c3508fc8d1e739d1b42d35
TommyB <tommyb@frostgaming.com>
2021-01-22 00:33:14 -0600

    Fix issue where cracked game fetch task didn't work and improve config

--- rudycrackwatch/rudycrackwatch.py
+++ rudycrackwatch/rudycrackwatch.py
@@ -11,35 +11,34 @@ from redbot.core.utils import menus
 class RudyCrackwatch(commands.Cog, name="Crackwatch Watcher"):
     def __init__(self, bot: discord.Client):
         default_global = {
-            "last_cracked_game": None
-        }
-
-        default_guild = {
-            "watch_channel": None
+            "last_cracked_game": None,
+            "watched_channels": []
         }
 
         self.bot = bot
         self.config = Config.get_conf(self, 45599)
-        self.config.register_guild(**default_guild)
         self.config.register_global(**default_global)
         self.game_check_task = self.bot.loop.create_task(self.fetch_cracked_games())
 
     async def fetch_cracked_games(self):
         while 1:
             data = None
-            async with aiohttp.ClientSession() as session:
-                async with session.get('https://api.crackwatch.com/api/games?page=0&sort_by=crack_date&is_cracked=true') as res:
-                    data = await res.json()
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get('https://api.crackwatch.com/api/games?page=0&sort_by=crack_date&is_cracked=true') as res:
+                        data = await res.json()
+            except aiohttp.ClientError:
+                await asyncio.sleep(60)
+                continue
 
             last_title = await self.config.last_cracked_game()
             if last_title != data[0]['title']:
-                embed = await self.format_game_info(data)
-                for guild in self.bot.guilds:
-                    channel_id = await self.config.guild(guild).watch_channel()
-                    if channel_id in [channel.id for channel in guild.channels]:
-                        channel = await self.bot.get_channel(channel_id)
-                        if channel is not None:
-                            await channel.send(embed=embed)
+                embed = await self.format_game_info(data[0])
+                channels = await self.config.watched_channels()
+                for channel_id in channels:
+                    channel = self.bot.get_channel(channel_id)
+                    if channel is not None:
+                        await channel.send(embed=embed)
             last_title = data[0]['title']
             await self.config.last_cracked_game.set(last_title)
             await asyncio.sleep(60)
@@ -68,8 +67,10 @@ class RudyCrackwatch(commands.Cog, name="Crackwatch Watcher"):
 
     @commands.Cog.listener()
     async def on_guild_channel_delete(self, channel: discord.abc.GuildChannel):
-        if channel.id == await self.config.guild(channel.guild).watch_channel():
-            await self.config.guild(channel.guild).watch_channel.set(None)
+        channels = await self.config.watched_channels()
+        if channel.id in channels:
+            channels.remove(channel.id)
+            await self.config.watched_channels.set(channels)
 
     @commands.group()
     async def crackwatch(self, ctx: commands.Context):
@@ -79,17 +80,23 @@ class RudyCrackwatch(commands.Cog, name="Crackwatch Watcher"):
     @crackwatch.command()
     @commands.mod_or_permissions(manage_guild=True)
     async def setalertchannel(self, ctx: commands.Context, channel: discord.TextChannel = None):
-        """Assigns a channel that will receive notifications whenever a new game is marked as cracked on Crackwatch."""
+        """Assigns a channel that will receive notifications whenever a new game is marked as cracked on Crackwatch.
+        If you'd like to unassign a channel, run this command in the channel that's already been set."""
+        channels = await self.config.watched_channels()
         if channel is None:
-            if await self.config.guild(ctx.guild).watch_channel is None:
+            if channel.id not in channels:
                 await ctx.send('Specify a channel to link Crackwatch notifications to.')
             else:
-                channel_id = await self.config.guild(ctx.guild).watch_channel()
-                channel = self.bot.get_channel(channel_id)
-                await self.config.guild(ctx.guild).watch_channel.set(None)
-                await ctx.send('This guild will no longer receive Crackwatch notifications.')
+                channels.remove(channel.id)
+                await self.config.watched_channels.set(channels)
+                await ctx.send('This channel will no longer receive Crackwatch notifications.')  # using ctx to send here because channel and ctx.channel would be identical
         else:
-            await self.config.guild(ctx.guild).watch_channel.set(channel.id)
+            if channel.id in channels:
+                await ctx.send(f'{channel.mention} already receives Crackwatch notifications.')
+                return
+
+            channels.append(channel.id)
+            await self.config.watched_channels.set(channels)
             await channel.send('This channel will now receive Crackwatch notifications.')
             await ctx.send(f'{channel.mention} will now receive Crackwatch notifications.')
 
